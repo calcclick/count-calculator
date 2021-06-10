52b697f6cde655a4b7b6fbd4b848c0748f17216b
awais <awais.ali@gmail.com>
2021-06-10 19:07:23 +0500

    change title

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => 'CountCalculator',//env('APP_NAME', 'Laravel'),
 
     /*
     |--------------------------------------------------------------------------
